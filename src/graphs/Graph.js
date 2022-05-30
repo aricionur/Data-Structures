c6b7b0553df37ad5df075860a4767b03740ed1c2
Onur ARICI <oarici@suntek.io>
2022-05-30 03:04:09 -0400

    Graph breadth-first traversal with queue

--- src/graphs/Graph.js
+++ src/graphs/Graph.js
@@ -1,3 +1,5 @@
+const { ArrayQueue } = require("../queue/ArrayQueue")
+
 class Graph {
   constructor() {
     this.adjacencyMap = {}
@@ -24,6 +26,38 @@ class Graph {
         }
       }
     })
+  }
+
+  traverseDepthFisrt(node) {
+    if (!this.adjacencyMap[node]) return
+
+    const visited = {}
+    this.traverseDepthFisrtRec(node, visited)
+  }
+
+  traverseDepthFisrtRec(node, visited) {
+    console.log(node)
+    visited[node] = 1
+
+    for (const adjacentNode of this.adjacencyMap[node]) {
+      if (!visited[adjacentNode]) this.traverseDepthFisrtRec(adjacentNode, visited)
+    }
+  }
+
+  traverseBreadthFisrt(node) {
+    if (!this.adjacencyMap[node]) return
+
+    const visited = {}
+    const queue = new ArrayQueue(10)
+    queue.enqueue(node)
+
+    while (!queue.isEmpty()) {
+      const currentNode = queue.dequeue()
+      console.log(currentNode)
+      visited[currentNode] = 1
+
+      this.adjacencyMap[currentNode].forEach(adjacentNode => !visited[adjacentNode] && queue.enqueue(adjacentNode))
+    }
   }
 }
 
